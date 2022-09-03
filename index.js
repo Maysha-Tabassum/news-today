9c8a81dd1e48696a37328282c5f5bc01b34eb8b0
Maysha-Tabassum <mayshatabassum77@gmail.com>
2022-09-03 12:48:40 +0600

    all data fetch commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hello from js');
+const loadNewsCategories = async() => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`
+    const res = await fetch (url);
+    const data = await res.json();
+    return data.data.news_category
+}
+
+const setAllMenu = async() => {
+    const data = await loadNewsCategories();
+    const menu = document.getElementById("all-menu");
+     for(const news of data){
+        // console.log(news.category_name)
+        const li = document.createElement("li");
+        li.innerHTML = `<a>${news.category_name}</a>`;
+        menu.appendChild(li);
+     }
+}
+setAllMenu();
+// loadNewsCategories();
+
